93b65db9b3773a84a469ec9e961d37327941aa3c
DekelAshush <102851893+DekelAshush@users.noreply.github.com>
2025-10-15 12:54:33 -0400

    fix: logo movment

--- Frontend/src/Components/Logo/Logo.jsx
+++ Frontend/src/Components/Logo/Logo.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import Tilt from 'react-parallax-tilt'
-import brain from './brain.png'
-import './Logo.css'
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
     return (
-       <div className="ma4 mt0">
-              <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-                  <div className="Tilt-inner pa3">
-                      <img alt='logo' src={brain}/>
-                  </div>
-
-              </Tilt>
-       </div>
+        <div className="ma4 mt0">
+            <Tilt
+                className="Tilt br2 shadow-2"
+                tiltMaxAngleX={55}
+                tiltMaxAngleY={55}
+                style={{ height: 150, width: 150 }}
+            >
+                <div className="Tilt-inner pa3">
+                    <img alt="logo" src={brain} style={{ width: '80%' }} />
+                </div>
+            </Tilt>
+        </div>
     );
-}
+};
 
 export default Logo;
